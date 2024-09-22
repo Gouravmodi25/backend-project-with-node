fc0b7943253451bba7803386ca75c029be1cb29e
Gouravmodi25 <gouravmodi39@gmail.com>
2024-09-22 09:55:24 +0530

    add token

---
+++ src/db/connection.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const DB_NAME = require("../constant");
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      "Database is connected to server at",
+      connectionInstance.connection.host
+    );
+  } catch (error) {
+    console.log("MongoDB connection Error:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const connectDB = require("./db/connection");
+require("dotenv").config({ path: "./env" });
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
+
+// Data connection to server
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is started at port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection Failed", error);
+  });
